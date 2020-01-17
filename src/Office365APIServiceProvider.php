6e1fa11cc329cac717f135dbbda703975f1a51b6
Jake Oandasan <j.oandasan@confrix.com>
2020-01-17 16:51:31 +0800

    Add process to modify .env file

--- src/Office365APIServiceProvider.php
+++ src/Office365APIServiceProvider.php
@@ -15,6 +15,18 @@ class Office365APIServiceProvider extends ServiceProvider
     {
         //register our controller
         $this->app->make('jakeoandasan\office365api\MicrosoftGraphController');
+		
+		//set our .env variables
+        $variables =
+            "\n\n" .
+            "\nOAUTH_APP_ID=" .
+            "\nOAUTH_APP_PASSWORD=" .
+            "\nOAUTH_REDIRECT_URI=" .
+            "\nOAUTH_SCOPES=" .
+            "\nOAUTH_AUTHORITY=" .
+            "\nOAUTH_AUTHORIZE_ENDPOINT=" .
+            "\nOAUTH_TOKEN_ENDPOINT=";
+        file_put_contents($this->app->environmentFilePath(), $variables, FILE_APPEND);
     }
 
     /**
